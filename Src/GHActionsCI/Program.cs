8988923601d6553f3bbdf7aee725dd7b003a63f0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-09-02 20:58:42 -0300

    First commit.

---
+++ Src/GHActionsCI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GHActionsCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
